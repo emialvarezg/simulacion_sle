0876776487b649ece6078013bdf512e322cef54b
emialvarezg <119767873+emialvarezg@users.noreply.github.com>
2024-10-19 10:57:07 -0600

    Update simulacion_sle.py

--- simulacion_sle.py
+++ simulacion_sle.py
@@ -4,12 +4,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cmath
 
+#función para simular una variable aleatoria Rademacher
 def R(p):
     if np.random.uniform() < p:
         return 1
     else:
         return -1
 
+#función de trazado h_k
 def h(z,d,dk):
     v = (d**2)/dk
     if d > 0:
@@ -21,6 +23,7 @@ def h(z,d,dk):
     s = complex(z - 2*np.sqrt((dk*a)/(1-a)))
     return (r**(1-a))*(s**a)
 
+#función para evaluar la composición de k funciones de trazado
 def hk(z0,k,dk,d):
     for i in range(k):
         z = h(z0,d[k-1 - i],dk)
@@ -29,28 +32,22 @@ def hk(z0,k,dk,d):
 
 #simular n pasos de sle(kappa)
 kappa = 3
-n=15000
+n=10000
 Delta_k = 1/n
 
+#simular incrementos de la función de conducción
+#(incrementos de un movimiento browniano de varianza kappa)
 d = [R(1/2)*np.sqrt(kappa*Delta_k) for i in range(n)]
-u = [sum(d[:i+1]) for i in range(len(d))]
-#u = [np.exp(i) for i in u]
-z0 = 0
-sle=[z0]
 
+#punto inicial
+z0 = 0
+sle=np.empty(n,dtype=complex)
+sle[0]=z0
 
 for k in range(1,n):
-    #z = hk(z0,k,1/n,d)
-    z = hk(z0,k,Delta_k,d)
-    sle.append(z)
-
-sle1=[]
-sle2=[]
-for z in sle:
-    sle1.append(z.real)
-    sle2.append(z.imag)
+    sle[k] = hk(z0,k,Delta_k,d)
 
 #gráfica
-plt.plot(sle1,sle2,color='k',linewidth=0.5)
+plt.plot(sle.real,sle.imag,linewidth=0.5)
 
 
